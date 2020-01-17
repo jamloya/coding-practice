861258f00fc8e6a57a6266747ddba006cdb27066
jatin kumar <42140725+jamloya@users.noreply.github.com>
2020-01-17 21:50:56 +0530

    Update single number 2.cpp

--- bit maninpulation/single number 2.cpp
+++ bit maninpulation/single number 2.cpp
@@ -4,7 +4,7 @@ public:
         int ones=0,twos=0;
         for(auto i:nums)
         {
-            /* if number is appear one or when it appears for third time 
+            /* if number appears for first time or when it appears for third time 
             remove it from twos */
             ones=(ones^i) & ~twos; 
             /*if number appear twice add it to twos
